27ccc2ea3308ea5006c42f29470d08f5b7255d5f
Chris Maggs <git@maggsweb.co.uk>
2026-04-21 22:44:10 +0100

    Update & Fix tests

--- tests/DeleteTest.php
+++ tests/DeleteTest.php
@@ -6,15 +6,14 @@ include_once 'BaseTestCase.php';
 
 class DeleteTest extends BaseTestCase
 {
-    public function testSetupDelete()
+    public function setUp(): void
     {
+        parent::setUp();
         $this->db->query('DELETE FROM address')->execute();
-        $this->assertEquals(0, $this->countAddresses());
         $this->db->query('INSERT INTO address VALUES (null, 1, "Clare address", "CF10");')->execute();
         $this->db->query('INSERT INTO address VALUES (null, 1, "Clare additional address", "CF20");')->execute();
         $this->db->query('INSERT INTO address VALUES (null, 2, "Chris main address", "CF10");')->execute();
         $this->db->query('INSERT INTO address VALUES (null, 2, "Chris additional address", "CF20");')->execute();
-        $this->assertEquals(4, $this->countAddresses());
     }
 
     /**
@@ -32,15 +31,13 @@ class DeleteTest extends BaseTestCase
 
     /**
      * @test
-     *
-     * @depends deleteUsingAWhereArray
      */
     public function deleteUsingAWhereString()
     {
         $delete = $this->db->delete('address', "WHERE address LIKE '%Chris%'");
         $this->assertTrue($delete);
-        $this->assertEquals(1, $this->db->numRows());
-        $this->assertEquals(1, $this->countAddresses());
+        $this->assertEquals(2, $this->db->numRows());
+        $this->assertEquals(2, $this->countAddresses());
     }
 
     /**
